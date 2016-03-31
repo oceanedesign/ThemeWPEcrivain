71d2a61e9306f2a670553e592df6ab9643607bea
Arthur Brugiere <arbr1@hotmail.fr>
2016-03-31 17:05:57 +0200

    Dbt CMB2 Ecrivain => Doublon siècle

--- lib/metaboxes.php
+++ lib/metaboxes.php
@@ -21,3 +21,33 @@ if ( file_exists(get_template_directory() . '/cmb2/init.php' ) ) {
 	require_once get_template_directory() . '/CMB2/init.php';
 }
 
+/*===================================================================
+ x                                                                  x
+ x                     		Ecrivain			                    x
+ x                                                                  x
+===================================================================*/
+
+add_action( 'cmb2_admin_init', 'ecrivain_metabox' );
+function ecrivain_metabox() {
+
+	// Start with an underscore to hide fields from custom fields list
+	$prefix = '_ecrivain_';
+
+	$cmb_ecrivain = new_cmb2_box( array(
+		'id'            => $prefix . 'metabox',
+		'title'         => __( 'Ecrivain Informations', 'starter' ),
+		'object_types'  => array( 'ecrivain')
+	) );
+	
+	$cmb_ecrivain->add_field( array(
+	    'name'     => 'siecle',
+	    'desc'     => 'Siècle de l\'auteur',
+	    'id'       => $prefix . 'siecle',
+	    'taxonomy' => 'siecle', // Enter Taxonomy Slug
+	    'type'     => 'taxonomy_radio',
+	    // Optional:
+	    'options' => array(
+	        'no_terms_text' => 'Sorry, no terms could be found.' // Change default text. Default: "No terms"
+	    ),
+	) );
+}
--- lib/taxonomies.php
+++ lib/taxonomies.php
@@ -76,3 +76,42 @@ function annee_sortie_taxonomy() {
 
 }
 add_action( 'init', 'annee_sortie_taxonomy', 0 );
+
+// Register Custom Taxonomy
+function siecle_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Siècles', 'Taxonomy General Name', 'starter' ),
+		'singular_name'              => _x( 'Siècle', 'Taxonomy Singular Name', 'starter' ),
+		'menu_name'                  => __( 'Siècle', 'starter' ),
+		'all_items'                  => __( 'All Siècles', 'starter' ),
+		'parent_item'                => __( 'Parent Siècle', 'starter' ),
+		'parent_item_colon'          => __( 'Parent Siècle:', 'starter' ),
+		'new_item_name'              => __( 'New Siècle', 'starter' ),
+		'add_new_item'               => __( 'Add New Siècle', 'starter' ),
+		'edit_item'                  => __( 'Edit Siècle', 'starter' ),
+		'update_item'                => __( 'Update Siècle', 'starter' ),
+		'view_item'                  => __( 'View Siècle', 'starter' ),
+		'separate_items_with_commas' => __( 'Separate Siècles with commas', 'starter' ),
+		'add_or_remove_items'        => __( 'Add or remove Siècles', 'starter' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'starter' ),
+		'popular_items'              => __( 'Popular Siècles', 'starter' ),
+		'search_items'               => __( 'Search Siècles', 'starter' ),
+		'not_found'                  => __( 'Not Found', 'starter' ),
+		'no_terms'                   => __( 'No Siècles', 'starter' ),
+		'items_list'                 => __( 'Siècles list', 'starter' ),
+		'items_list_navigation'      => __( 'Siècles list navigation', 'starter' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'siecle', array( 'ecrivain' ), $args );
+
+}
+add_action( 'init', 'siecle_taxonomy', 0 );

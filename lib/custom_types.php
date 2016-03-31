cbd319769217ac33909d063c07b8d4e642d2be73
Arthur Brugiere <arbr1@hotmail.fr>
2016-03-31 16:48:04 +0200

    Taxonomies

--- lib/custom_types.php
+++ lib/custom_types.php
@@ -1,1 +1,110 @@
 <?php
+// Register Custom Post Type
+function custom_ecrivain_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Ecrivains', 'Post Type General Name', 'starter' ),
+		'singular_name'         => _x( 'Ecrivain', 'Post Type Singular Name', 'starter' ),
+		'menu_name'             => __( 'Ecrivain', 'starter' ),
+		'name_admin_bar'        => __( 'Ecrivain', 'starter' ),
+		'archives'              => __( 'Ecrivain Archives', 'starter' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'starter' ),
+		'all_items'             => __( 'All Ecrivains', 'starter' ),
+		'add_new_item'          => __( 'Add New Ecrivain', 'starter' ),
+		'add_new'               => __( 'Add Ecrivain', 'starter' ),
+		'new_item'              => __( 'New Ecrivain', 'starter' ),
+		'edit_item'             => __( 'Edit Ecrivain', 'starter' ),
+		'update_item'           => __( 'Update Ecrivain', 'starter' ),
+		'view_item'             => __( 'View Ecrivain', 'starter' ),
+		'search_items'          => __( 'Search Ecrivain', 'starter' ),
+		'not_found'             => __( 'Not found', 'starter' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'starter' ),
+		'featured_image'        => __( 'Featured Image', 'starter' ),
+		'set_featured_image'    => __( 'Set featured image', 'starter' ),
+		'remove_featured_image' => __( 'Remove featured image', 'starter' ),
+		'use_featured_image'    => __( 'Use as featured image', 'starter' ),
+		'insert_into_item'      => __( 'Insert into écrivain', 'starter' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this écrivain', 'starter' ),
+		'items_list'            => __( 'Ecrivains list', 'starter' ),
+		'items_list_navigation' => __( 'Ecrivains list navigation', 'starter' ),
+		'filter_items_list'     => __( 'Filter écrivains list', 'starter' ),
+	);
+	$args = array(
+		'label'                 => __( 'Ecrivain', 'starter' ),
+		'description'           => __( 'Ecrivain', 'starter' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
+		'taxonomies'            => array( 'category', 'langue' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-businessman',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'ecrivain', $args );
+
+}
+add_action( 'init', 'custom_ecrivain_type', 0 );
+
+// Register Custom Post Type
+function custom_livre_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Livres', 'Post Type General Name', 'starter' ),
+		'singular_name'         => _x( 'Livre', 'Post Type Singular Name', 'starter' ),
+		'menu_name'             => __( 'Livre', 'starter' ),
+		'name_admin_bar'        => __( 'Livre', 'starter' ),
+		'archives'              => __( 'Livre Archives', 'starter' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'starter' ),
+		'all_items'             => __( 'All Livres', 'starter' ),
+		'add_new_item'          => __( 'Add New Livre', 'starter' ),
+		'add_new'               => __( 'Add Livre', 'starter' ),
+		'new_item'              => __( 'New Livre', 'starter' ),
+		'edit_item'             => __( 'Edit Livre', 'starter' ),
+		'update_item'           => __( 'Update Livre', 'starter' ),
+		'view_item'             => __( 'View Livre', 'starter' ),
+		'search_items'          => __( 'Search Livre', 'starter' ),
+		'not_found'             => __( 'Not found', 'starter' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'starter' ),
+		'featured_image'        => __( 'Featured Image', 'starter' ),
+		'set_featured_image'    => __( 'Set featured image', 'starter' ),
+		'remove_featured_image' => __( 'Remove featured image', 'starter' ),
+		'use_featured_image'    => __( 'Use as featured image', 'starter' ),
+		'insert_into_item'      => __( 'Insert into livre', 'starter' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this livre', 'starter' ),
+		'items_list'            => __( 'Livres list', 'starter' ),
+		'items_list_navigation' => __( 'Livres list navigation', 'starter' ),
+		'filter_items_list'     => __( 'Filter livres list', 'starter' ),
+	);
+	$args = array(
+		'label'                 => __( 'Livre', 'starter' ),
+		'description'           => __( 'Livre', 'starter' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', 'post-formats', ),
+		'taxonomies'            => array( 'category', 'langue', 'annee_sortie' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => ' dashicons-book-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'livre', $args );
+
+}
+add_action( 'init', 'custom_livre_type', 0 );
--- lib/taxonomies.php
+++ lib/taxonomies.php
@@ -1,1 +1,78 @@
 <?php
+// Register Custom Taxonomy
+function langue_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Langues', 'Taxonomy General Name', 'starter' ),
+		'singular_name'              => _x( 'Langue', 'Taxonomy Singular Name', 'starter' ),
+		'menu_name'                  => __( 'Taxonomy', 'starter' ),
+		'all_items'                  => __( 'All Langues', 'starter' ),
+		'parent_item'                => __( 'Parent Langue', 'starter' ),
+		'parent_item_colon'          => __( 'Parent Langue:', 'starter' ),
+		'new_item_name'              => __( 'New Langue Name', 'starter' ),
+		'add_new_item'               => __( 'Add New Langue', 'starter' ),
+		'edit_item'                  => __( 'Edit Langue', 'starter' ),
+		'update_item'                => __( 'Update Langue', 'starter' ),
+		'view_item'                  => __( 'View Langue', 'starter' ),
+		'separate_items_with_commas' => __( 'Separate Langues with commas', 'starter' ),
+		'add_or_remove_items'        => __( 'Add or remove Langues', 'starter' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'starter' ),
+		'popular_items'              => __( 'Popular langues', 'starter' ),
+		'search_items'               => __( 'Search Langues', 'starter' ),
+		'not_found'                  => __( 'Not Found', 'starter' ),
+		'no_terms'                   => __( 'No Langues', 'starter' ),
+		'items_list'                 => __( 'Langues list', 'starter' ),
+		'items_list_navigation'      => __( 'Langues list navigation', 'starter' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'langue', array( 'post', 'ecrivain', 'livre' ), $args );
+
+}
+add_action( 'init', 'langue_taxonomy', 0 );
+
+// Register Custom Taxonomy
+function annee_sortie_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Années de sortie', 'Taxonomy General Name', 'starter' ),
+		'singular_name'              => _x( 'Année de sortie', 'Taxonomy Singular Name', 'starter' ),
+		'menu_name'                  => __( 'Taxonomy', 'starter' ),
+		'all_items'                  => __( 'All Années de sortie', 'starter' ),
+		'parent_item'                => __( 'Parent Année de sortie', 'starter' ),
+		'parent_item_colon'          => __( 'Parent Année de sortie:', 'starter' ),
+		'new_item_name'              => __( 'New Année de sortie Name', 'starter' ),
+		'add_new_item'               => __( 'Add New Année de sortie', 'starter' ),
+		'edit_item'                  => __( 'Edit Année de sortie', 'starter' ),
+		'update_item'                => __( 'Update Année de sortie', 'starter' ),
+		'view_item'                  => __( 'View Année de sortie', 'starter' ),
+		'separate_items_with_commas' => __( 'Separate Années de sortie with commas', 'starter' ),
+		'add_or_remove_items'        => __( 'Add or remove Années de sortie', 'starter' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'starter' ),
+		'popular_items'              => __( 'Popular Années de sortie', 'starter' ),
+		'search_items'               => __( 'Search Années de sortie', 'starter' ),
+		'not_found'                  => __( 'Not Found', 'starter' ),
+		'no_terms'                   => __( 'No Années de sortie', 'starter' ),
+		'items_list'                 => __( 'Années de sortie list', 'starter' ),
+		'items_list_navigation'      => __( 'Années de sortie list navigation', 'starter' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'annee_sortie', array( 'post', 'livre' ), $args );
+
+}
+add_action( 'init', 'annee_sortie_taxonomy', 0 );
